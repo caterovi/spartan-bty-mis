fcc7340d03ce7518fabd63c09f0c148f48b6a441
caterovi <cate.rovi02@gmail.com>
2026-04-18 20:20:33 +0800

    update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -10,6 +11,9 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   connectTimeout: 10000,
   acquireTimeout: 10000,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool.promise();
